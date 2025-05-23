0fa05b150c815512bfb8b1e47d53ff78d4928338
mihainrs <mmihai97@gmail.com>
2025-05-23 18:59:44 +0300

    Initial setup, home page

--- auth_with_roles/settings.py
+++ auth_with_roles/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'role_auth',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
